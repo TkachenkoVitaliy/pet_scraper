d6434b6518fcf52cb55e4dfbdae89d4d1d43a39d
TkachenkoVitaliy <tkachenko.java@yandex.ru>
2022-12-07 21:57:59 +0500

    add test save ScrapTask

--- src/main/java/ru/vtkachenko/pet_scraper/service/ScrapTasksService.java
+++ src/main/java/ru/vtkachenko/pet_scraper/service/ScrapTasksService.java
@@ -3,19 +3,34 @@ package ru.vtkachenko.pet_scraper.service;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.vtkachenko.pet_scraper.exception.NaNException;
 import ru.vtkachenko.pet_scraper.model.PreliminaryTask;
 import ru.vtkachenko.pet_scraper.model.ScrapTask;
+import ru.vtkachenko.pet_scraper.repository.ScrapTaskRepository;
 
 import java.io.IOException;
 
 @Service
 public class ScrapTasksService {
 
-    public int runScrapTask(ScrapTask task) throws IOException, NaNException {
+    private final ScrapTaskRepository repository;
+
+    @Autowired
+    public ScrapTasksService(ScrapTaskRepository repository) {
+        this.repository = repository;
+    }
+
+    public void saveScrapTask(ScrapTask scrapTask) {
+        repository.save(scrapTask);
+    }
+
+    public int runScrapTask(ScrapTask scrapTask) throws IOException, NaNException {
+        saveScrapTask(scrapTask);
+
         int result = 0;
-        String textResult = runPreliminaryTask(task);
+        String textResult = runPreliminaryTask(scrapTask);
 
         try {
             result = Integer.parseInt(textResult.replaceAll(" ", ""));
