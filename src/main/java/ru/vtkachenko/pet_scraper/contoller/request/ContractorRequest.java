2c105951879627cc5fa42915e5056da6e1aa0407
TkachenkoVitaliy <tkachenko.java@yandex.ru>
2022-12-08 22:26:20 +0500

    start work on Contractor Request and Service

---
+++ src/main/java/ru/vtkachenko/pet_scraper/contoller/request/ContractorRequest.java
@@ -0,0 +1,62 @@
+package ru.vtkachenko.pet_scraper.contoller.request;
+
+
+public class ContractorRequest {
+
+    private Long id;
+
+    private String name;
+
+    private String inn;
+
+    private Boolean isActive;
+
+    public ContractorRequest() {
+    }
+
+    public ContractorRequest(Long id, String name, String inn, Boolean isActive) {
+        this.id = id;
+        this.name = name;
+        this.inn = inn;
+        this.isActive = isActive;
+    }
+
+    public ContractorRequest(String name, String inn) {
+        this.name = name;
+        this.inn = inn;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getInn() {
+        return inn;
+    }
+
+    public void setInn(String inn) {
+        this.inn = inn;
+    }
+
+    public Boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
+    }
+
+
+}
---
+++ src/main/java/ru/vtkachenko/pet_scraper/service/ContractorService.java
@@ -0,0 +1,43 @@
+package ru.vtkachenko.pet_scraper.service;
+
+import org.springframework.stereotype.Service;
+import ru.vtkachenko.pet_scraper.contoller.request.ContractorRequest;
+import ru.vtkachenko.pet_scraper.exception.NotFoundObjectException;
+import ru.vtkachenko.pet_scraper.model.Contractor;
+import ru.vtkachenko.pet_scraper.repository.ContractorRepository;
+
+import java.util.List;
+
+@Service
+public class ContractorService {
+
+    private final ContractorRepository contractorRepository;
+
+
+    public ContractorService(ContractorRepository contractorRepository) {
+        this.contractorRepository = contractorRepository;
+    }
+
+    public void saveContractor(ContractorRequest contractorRequest) {
+        Contractor contractor = new Contractor();
+        contractor.setId(contractorRequest.getId() != null ? contractorRequest.getId() : null);
+        contractor.setName(contractorRequest.getName() != null ? contractorRequest.getName() : null);
+        contractor.setInn(contractorRequest.getInn() != null ? contractorRequest.getInn() : null);
+        contractor.setActive(contractorRequest.isActive() != null ? contractorRequest.isActive() : true);
+
+        contractorRepository.save(contractor);
+    }
+
+    public List<Contractor> getAllContractors() {
+        return contractorRepository.findAll();
+    }
+
+    public void updateContractor(ContractorRequest contractorRequest) throws NotFoundObjectException {
+        findById(contractorRequest.getId());
+        saveContractor(contractorRequest);
+    }
+    public Contractor findById(Long id) throws NotFoundObjectException {
+        return contractorRepository.findById(id)
+                .orElseThrow(() -> new NotFoundObjectException("Не удалось найти контрагента"));
+    }
+}
