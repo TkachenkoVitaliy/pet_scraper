4226fe9e6be37aeed062eb4761cb85e9c1bf64e1
TkachenkoVitaliy <tkachenko.java@yandex.ru>
2022-12-10 14:33:52 +0500

    added nomenclature repository

---
+++ src/main/java/ru/vtkachenko/pet_scraper/nomenclature/repository/NomenclatureRepository.java
@@ -0,0 +1,6 @@
+package ru.vtkachenko.pet_scraper.nomenclature.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NomenclatureRepository extends JpaRepository {
+}
