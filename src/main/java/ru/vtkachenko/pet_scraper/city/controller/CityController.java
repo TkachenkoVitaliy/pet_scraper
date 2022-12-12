454c441816f6aef16ab196611ca2bab66c0762e3
TkachenkoVitaliy <tkachenko.java@yandex.ru>
2022-12-12 22:01:47 +0500

    city changes

--- src/main/java/ru/vtkachenko/pet_scraper/city/controller/CityController.java
+++ src/main/java/ru/vtkachenko/pet_scraper/city/controller/CityController.java
@@ -5,25 +5,52 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.vtkachenko.pet_scraper.city.controller.dto.CityRequest;
-import ru.vtkachenko.pet_scraper.exception.NotFoundObjectException;
+import ru.vtkachenko.pet_scraper.city.controller.dto.CityResponse;
 import ru.vtkachenko.pet_scraper.city.model.City;
 import ru.vtkachenko.pet_scraper.city.service.CityService;
+import ru.vtkachenko.pet_scraper.exception.NotFoundObjectException;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @CrossOrigin(origins = "*")
 public class CityController {
 
     private final CityService cityService;
+    private final CityMapper cityMapper;
 
     @Autowired
-    public CityController(CityService cityService) {
+    public CityController(CityService cityService, CityMapper cityMapper) {
         this.cityService = cityService;
+        this.cityMapper = cityMapper;
     }
 
     @PostMapping("/cities")
-    public ResponseEntity createCity(@RequestBody CityRequest cityRequest){
+    public ResponseEntity<CityResponse> createCity(@RequestBody CityRequest cityRequest) {
+        City city = cityMapper.mapToEntity(cityRequest);
+        CityResponse cityResponse = cityMapper.mapFromEntity(cityService.saveCity(city));
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(cityResponse);
+    }
+
+    @GetMapping("/cities")
+    public ResponseEntity<List<CityResponse>> getAllCity() {
+        List<City> cities = cityService.getAllCities();
+        List<CityResponse> citiesResponse = cities.stream()
+                .map(cityMapper::mapFromEntity)
+                .toList();
+
+        return ResponseEntity.status(HttpStatus.OK).body(citiesResponse);
+    }
+
+    @GetMapping("/cities/{id}")
+    public ResponseEntity<CityResponse> getCityById(@PathVariable Long id) throws NotFoundObjectException {
+        // TODO придумать как отдавать на клиента обработанные ошибки
+        City city = cityService.getCityById(id);
+        CityResponse cityResponse = cityMapper.mapFromEntity(city);
+
+        return ResponseEntity.status(HttpStatus.OK).body(cityResponse);
     }
 
 }
